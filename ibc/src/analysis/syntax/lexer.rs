eaa883453533cc7fd537f5c67d58e1fe4ed68ee4
Filip Toth <filip.toth@accelley.com>
2024-11-04 14:37:21 -0500

    Lex

--- ibc/src/analysis/syntax/lexer.rs
+++ ibc/src/analysis/syntax/lexer.rs
@@ -1,4 +1,4 @@
-use std::vec;
+use std::{str::Chars, vec};
 
 #[derive(Debug)]
 pub struct SyntaxToken {
@@ -17,7 +17,70 @@ pub enum SyntaxTokenKind {
     MinusToken,
     StarToken,
     SlashToken,
+    IntegerToken(i64),
     IdentifierToken(String),
+
+    IfKeyword,
+    ThenKeyword,
+    EndKeyword,
+    ElseKeyword,
+    OutputKeyword,
+    ReturnKeyword,
+    FunctionKeyword,
+}
+
+fn lex_identifier_or_keyword(value: String) -> SyntaxToken {
+    let kind = match value.as_str() {
+        "if" => SyntaxTokenKind::IfKeyword,
+        "then" => SyntaxTokenKind::ThenKeyword,
+        "end" => SyntaxTokenKind::EndKeyword,
+        "else" => SyntaxTokenKind::ElseKeyword,
+        "output" => SyntaxTokenKind::OutputKeyword,
+        "return" => SyntaxTokenKind::ReturnKeyword,
+        "function" => SyntaxTokenKind::FunctionKeyword,
+        _ => SyntaxTokenKind::IdentifierToken(value)
+    };
+
+    SyntaxToken::new(kind) 
+}
+
+fn lex_rolling(iter: &mut Chars, current: char) -> SyntaxToken {
+    let mut value = current.to_string();
+    let is_numeric = current.is_numeric();
+
+    loop {
+        match iter.next() {
+            Some(next) => {
+                if !next.is_alphanumeric() {
+                    break;
+                }
+
+                if !next.is_numeric() && is_numeric {
+                    // rollback position to prev, or just undo
+                    // .next try running 123abs, this will return
+                    // token(123) token(bs), but the 2nd token
+                    // should also indlude the 'a'
+                    break;
+                }
+
+                value.push(next);
+            },
+            None => { break; }
+        };
+    }
+
+    if is_numeric {
+        let int_value = value.parse();
+        match int_value {
+            Ok(v) => {
+                let kind = SyntaxTokenKind::IntegerToken(v);
+                return SyntaxToken::new(kind);
+            },
+            Err(_) => unreachable!()
+        }
+    }
+
+    lex_identifier_or_keyword(value)
 }
 
 pub fn lex(content: String) -> Vec<SyntaxToken> {
@@ -38,23 +101,9 @@ pub fn lex(content: String) -> Vec<SyntaxToken> {
             '/' => SyntaxToken::new(SyntaxTokenKind::SlashToken),
             ' ' => continue,
             _ => {
-                match current_value {
-                    None => {
-                        current_value = Some(current.to_string());
-                    },
-                    Some(ref mut c) => c.push(current)
-                }
-
-                continue;
+                lex_rolling(&mut chars, current)
             }
         };
-
-        if let Some(ref mut c) = current_value {
-            let identifier = SyntaxToken::new(SyntaxTokenKind::IdentifierToken(c.clone()));
-            tokens.push(identifier);
-
-            current_value = None;
-        }
 
         tokens.push(token);
     }
