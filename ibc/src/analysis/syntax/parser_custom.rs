92f2a5b97ccfab7aec64db09f399a5ee7aa8c40b
Filip Toth <filip.toth@accelley.com>
2024-11-06 15:23:50 -0500

    Add expression parser

--- ibc/src/analysis/syntax/parser_custom.rs
+++ ibc/src/analysis/syntax/parser_custom.rs
@@ -1,8 +1,10 @@
 use std::{cell::RefCell, iter::Peekable, rc::Rc, slice::Iter};
 
 use super::lexer::{LexerToken, LexerTokenKind};
-type LexerTokens<'a> = Rc<RefCell<Peekable<Iter<'a, LexerToken>>>>;
 
+type LexerTokens<'a> = Peekable<Iter<'a, LexerToken>>;
+
+#[derive(Debug)]
 pub struct ParsedToken {
     pub kind: ParsedTokenKind,
 }
@@ -13,6 +15,7 @@ impl ParsedToken {
     }
 }
 
+#[derive(Debug)]
 pub enum Operator {
     Addition,
     Subtraction,
@@ -22,7 +25,11 @@ pub enum Operator {
     Equality,
 }
 
+#[derive(Debug)]
 pub enum ParsedTokenKind {
+    Scope {
+        subtokens: Vec<ParsedToken>
+    },
     ReferenceExpression(String),
     IntegerLiteralExpression(i64),
     BinaryExpression {
@@ -36,130 +43,150 @@ pub enum ParsedTokenKind {
     },
 }
 
-fn parse_binary_expression(rc_tokens: LexerTokens, parent_precedence: usize) -> Option<ParsedToken> {
-    let mut tokens = rc_tokens.borrow_mut();
-    let next_peek = tokens.next();
+struct Parser<'a> {
+    tokens: LexerTokens<'a>
+}
 
-    let Some(next_peek) = next_peek else {
-        return None;
-    };
+impl<'a> Parser<'a> {
+    fn new(tokens: LexerTokens<'a>) -> Self {
+        Parser { tokens: tokens }
+    }
 
-    let unary_precedence = next_peek.kind.unary_operator_precedence();
-    let mut lhs = if unary_precedence != 0 {
-        // unary expression
-        let next = tokens.next().unwrap();
-        let operator = match parse_operator(next) {
-            Some(o) => o,
-            None => return None,
+    fn parse_binary_expression(&mut self, parent_precedence: usize) -> Option<ParsedToken> {
+        let unary_precedence = match self.tokens.peek() {
+            Some(t) => t.kind.unary_operator_precedence(),
+            None => return None
+        };
+    
+        let mut lhs = if unary_precedence != 0 {
+            // unary expression
+            let next = self.tokens.next().unwrap();    
+            let operator = match self.parse_operator(next) {
+                Some(o) => o,
+                None => return None,
+            };
+    
+            let rhs = match self.parse_binary_expression(0) {
+                Some(r) => r,
+                None => return None,
+            };
+    
+            let unary_kind = ParsedTokenKind::UnaryExpression {
+                op: operator,
+                rhs: Box::new(rhs),
+            };
+    
+            let token = ParsedToken::new(unary_kind);
+            Some(token)
+        } else {
+            self.parse_primary_expression()
         };
 
-        let rhs = match parse_binary_expression(rc_tokens.clone(), 0) {
-            Some(r) => r,
-            None => return None,
-        };
+        // println!("lhs: {:?}", lhs);
+    
+        while let Some(lhs_token) = lhs.take() {
+            let precedence = match self.tokens.peek() {
+                Some(t) => {
+                    // println!("Peeking for token precedence {:?}", t);
+                    t.kind.binary_operator_precedence()
+                },
+                None => {
+                    lhs = Some(lhs_token);
+                    break;
+                },
+            };
 
-        let unary_kind = ParsedTokenKind::UnaryExpression {
-            op: operator,
-            rhs: Box::new(rhs),
-        };
+            // println!("Binary precedence {:?}", precedence);
 
-        let token = ParsedToken::new(unary_kind);
+            if precedence == 0 || precedence <= parent_precedence {
+                break;
+            }
+    
+            let operator_token = match self.tokens.next() {
+                Some(t) => t,
+                None => break
+            };
+    
+            let operator = match self.parse_operator(operator_token) {
+                Some(o) => o,
+                None => return None
+            };
+    
+            let rhs = match self.parse_binary_expression(precedence) {
+                Some(r) => r,
+                None => break,
+            };
+    
+            let bin_expr_kind = ParsedTokenKind::BinaryExpression { lhs: Box::new(lhs_token), op: operator, rhs: Box::new(rhs) };
+            let bin_expr = ParsedToken::new(bin_expr_kind);
+            lhs = Some(bin_expr);
+        }
+    
+        lhs
+    }
+    
+    fn parse_primary_expression(&mut self) -> Option<ParsedToken> {
+        let next = match self.tokens.next() {
+            Some(n) => n,
+            None => return None
+        };
+    
+        let kind = match &next.kind {
+            LexerTokenKind::IdentifierToken(id) => ParsedTokenKind::ReferenceExpression(id.clone()),
+            LexerTokenKind::IntegerLiteralToken(val) => ParsedTokenKind::IntegerLiteralExpression(val.clone()),
+            _ => return None
+        };
+    
+        let token = ParsedToken::new(kind);
         Some(token)
-    } else {
-        parse_primary_expression(rc_tokens.clone())
-    };
-
-    while lhs.is_some() {
-        let precedence = next_peek.kind.binary_operator_precedence();
-        if precedence == 0 || precedence <= parent_precedence {
-            break;
-        }
-
-        let operator_token = match tokens.next() {
-            Some(t) => t,
-            None => return None
-        };
-
-        let operator = match parse_operator(operator_token) {
-            Some(o) => o,
-            None => return None
-        };
-
-        let rhs = match parse_binary_expression(rc_tokens.clone(), precedence) {
-            Some(r) => r,
-            None => return None,
-        };
-
-        let lhs_owned = match lhs.take() {
-            Some(l) => l,
-            None => return None,
-        };
-
-        let bin_expr_kind = ParsedTokenKind::BinaryExpression { lhs: Box::new(lhs_owned), op: operator, rhs: Box::new(rhs) };
-        let bin_expr = ParsedToken::new(bin_expr_kind);
-        lhs = Some(bin_expr);
     }
-
-    None
-}
-
-fn parse_primary_expression(rc_tokens: LexerTokens) -> Option<ParsedToken> {
-    let mut tokens = rc_tokens.borrow_mut();
-    let next = match tokens.next() {
-        Some(n) => n,
-        None => return None
-    };
-
-    let kind = match &next.kind {
-        LexerTokenKind::IdentifierToken(id) => ParsedTokenKind::ReferenceExpression(id.clone()),
-        LexerTokenKind::IntegerLiteralToken(val) => ParsedTokenKind::IntegerLiteralExpression(val.clone()),
-        _ => return None
-    };
-
-    let token = ParsedToken::new(kind);
-    Some(token)
-}
-
-fn parse_operator(token: &LexerToken) -> Option<Operator> {
-    match token.kind {
-        LexerTokenKind::PlusToken => Some(Operator::Addition),
-        LexerTokenKind::MinusToken => Some(Operator::Subtraction),
-        LexerTokenKind::StarToken => Some(Operator::Multiplication),
-        LexerTokenKind::SlashToken => Some(Operator::Division),
-        _ => None,
+    
+    fn parse_operator(&self, token: &LexerToken) -> Option<Operator> {
+        match token.kind {
+            LexerTokenKind::PlusToken => Some(Operator::Addition),
+            LexerTokenKind::MinusToken => Some(Operator::Subtraction),
+            LexerTokenKind::StarToken => Some(Operator::Multiplication),
+            LexerTokenKind::SlashToken => Some(Operator::Division),
+            _ => None,
+        }
     }
-}
-
-fn parse_statement(rc_tokens: LexerTokens) -> Option<ParsedToken> {
-    let mut tokens = rc_tokens.borrow_mut();
-    let peek = tokens.peek();
-
-    let Some(peek) = peek else { return None };
-
-    match peek.kind {
-        LexerTokenKind::OutputKeyword => {}
-        LexerTokenKind::IfKeyword => {}
-        LexerTokenKind::FunctionKeyword => {}
-        LexerTokenKind::IdentifierToken(_) | LexerTokenKind::IntegerLiteralToken(_) => {
-            // parse expression
+    
+    fn parse_statement(&mut self) -> Option<ParsedToken> {
+        let peek = self.tokens.peek();
+    
+        let Some(peek) = peek else { return None };
+        match peek.kind {
+            LexerTokenKind::OutputKeyword => None,
+            LexerTokenKind::IfKeyword => None,
+            LexerTokenKind::FunctionKeyword => None,
+            _ => {
+                let bin_token = self.parse_binary_expression(0);
+                bin_token
+            }
         }
-        _ => unreachable!(),
-    };
+    }
+    
+    fn parse_global_scope(&mut self) -> Option<ParsedToken> {
+        let mut parsed: Vec<ParsedToken> = Vec::new();
+        loop {
+            let statement = self.parse_statement();
+            match statement {
+                Some(s) => parsed.push(s),
+                None => break,
+            };
+        };
 
-    None
-}
+        let scope_kind = ParsedTokenKind::Scope { subtokens: parsed };
 
-fn parse_global_scope(rc_tokens: LexerTokens) {
-    loop {
-        let statement = parse_statement(rc_tokens.clone());
-        match statement {
-            Some(s) => {}
-            None => break,
-        }
+        let token = ParsedToken::new(scope_kind);
+        Some(token)
     }
 }
 
 pub fn parse_tokens(tokens: Vec<LexerToken>) {
     let mut iter = tokens.iter().peekable();
-    parse_global_scope(Rc::new(RefCell::new(iter)));
+    let mut parser = Parser::new(iter);
+    let res = parser.parse_global_scope();
+
+    println!("{:#?}", res);
 }
--- manual_test/src/main.rs
+++ manual_test/src/main.rs
@@ -5,7 +5,6 @@ extern crate ibc;
 fn main() {
     let content = fs::read_to_string("lexer_test.ib").unwrap();
     let tokens = ibc::analysis::syntax::lexer::lex(content);
-    for token in tokens {
-        println!("{:?}", token)
-    }
+
+    ibc::analysis::syntax::parser_custom::parse_tokens(tokens);
 }
